f3053ecea5f778bfbaf924de691e02139acf2d84
guo <283316914@qq.com>
2022-06-27 08:32:59 +0800

    20220624

---
+++ cloud-gateway-gateway9527/src/main/java/com/guoyf/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.guoyf.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author guoyf
+ * @Description
+ * @date 2022/6/27 8:20
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run( GateWayMain9527.class,args);
+    }
+}
