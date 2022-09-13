bc1d0f6901b2dcb207ffd4e92f1f366e6743bf06
guo <283316914@qq.com>
2022-09-14 07:54:20 +0800

    20220914

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/guoyf/springcloud/alibaba/MainApp8401.java
@@ -0,0 +1,16 @@
+package com.guoyf.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author guoyf
+ * @Description
+ * @date 2022/9/12 19:22
+ */
+@SpringBootApplication
+public class MainApp8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/guoyf/springcloud/alibaba/controller/FlowLimitController.java
@@ -0,0 +1,25 @@
+package com.guoyf.springcloud.alibaba.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author guoyf
+ * @Description
+ * @date 2022/9/12 19:46
+ */
+@RestController
+@Slf4j
+public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        return "------testB";
+    }
+
+}
