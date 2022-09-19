cdd5b45aa7944e2290812face7bfb781d38ff638
guoyf <283316914@qq.com>
2022-09-19 18:40:44 +0800

    20220919

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/guoyf/springcloud/alibaba/controller/CircleBreakerController.java
@@ -0,0 +1,39 @@
+package com.guoyf.springcloud.alibaba.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.guoyf.springcloud.entities.CommonResult;
+import com.guoyf.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RestController
+public class CircleBreakerController {
+
+    public static final String SERVICE_URL = "http://nacos-payment-provider";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/consumer/fallback/{id}")
+    @SentinelResource(value = "fallback") //没有配置
+    //@SentinelResource(value = "fallback",blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
+    //@SentinelResource(value = "fallback",fallback = "handlerFallback",blockHandler = "blockHandler", exceptionsToIgnore = {IllegalArgumentException.class})
+    public CommonResult<Payment> fallback(@PathVariable("id") Long id) {
+        CommonResult<Payment> result = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/{id}", CommonResult.class,id);
+        if (id == 4) {
+            throw new IllegalArgumentException ("IllegalArgumentException,非法参数异常....");
+        }else if (result.getData() == null) {
+            throw new NullPointerException ("NullPointerException,该ID没有对应记录,空指针异常");
+        }
+        return result;
+    }
+
+
+
+}
